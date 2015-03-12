cf0e16f469e8f0101e0362147705fba677b52ef7
jfernandez14 <jfernandez@itconvergence.com>
2015-03-12 11:21:10 -0600

    Create CRONTAB_EXAMPLE.sh
    Show information regarding the configuration of a crontab entry.

---
+++ CRONTAB_EXAMPLE.sh
@@ -0,0 +1,8 @@
+# ITC Daily Hceck APPDS
+ 1 4 * * * /u01/home/applprod/itc/cron/apps_daily_checklist/apps_daily_check.sh PROD od12walla0
+#| | | | |
+#| | | | +----- day of week (0 - 6) (Sunday=0)
+#| | | +------- month (1 - 12)
+#| | +--------- day of month (1 - 31)
+#| +----------- hour (0 - 23)
+#+------------- min (0 - 59)
